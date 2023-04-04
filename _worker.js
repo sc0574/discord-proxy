324b7cb45bd6cdee0c473f10eacd06efb65f677b
sc0574 <673116067@qq.com>
2023-04-04 21:59:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,13 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="cdn.discordapp.com";
+      if (url.pathname.startsWith('/discordmedia')) {
+        url.pathname=url.pathname.replace("/discordmedia","");
+        url.hostname="media.discordapp.net";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
+ 
       // Otherwise, serve the static assets.
       return env.ASSETS.fetch(request);
     }
