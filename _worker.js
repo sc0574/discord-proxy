a4ad10ce76d75dbdf3a4c0471bfa8706c31db6be
sc0574 <673116067@qq.com>
2023-04-08 13:49:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,6 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/discordmedia')) {
-        url.pathname=url.pathname.replace("/discordmedia","");
-        url.hostname="media.discordapp.net";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
       if (url.pathname.startsWith('/')) {
         url.hostname="discord.com";
         let new_request=new Request(url,request);
